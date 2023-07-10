107fd93c5434a93d861bd48eb487558d62d9fdf2
Titapon Keawbua <64160047@go.buu.ac.th>
2023-07-10 20:30:07 +0700

    said byebye no play

--- src/main/java/com/mycompany/oxlab1/Oxlab1.java
+++ src/main/java/com/mycompany/oxlab1/Oxlab1.java
@@ -4,14 +4,61 @@
 
 package com.mycompany.oxlab1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author minnie
  */
 public class Oxlab1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
+    public int row;
+    public int col;
+    Scanner kb = new Scanner(System.in);
+    public boolean play = false;
+    public String start;
+    public boolean end;
+    public String turn;
+    public String[][] board = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
+
+    
+
+ 
+    public void showBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+
+            }
+            System.out.println();
+
+        }
 
     }
+
+
+    public void lean() {
+        showBoard();
+
+    }
+    
+    public static void main(String[] args) {
+
+         Oxlab1 project = new  Oxlab1();
+        Scanner kb = new Scanner(System.in);
+        if (project.play == false) {
+            System.out.println("--- Bye Bye ---");
+            return;
+        }
+
+        project.turn = "x";
+        while (project.play) {
+            project.lean();
+        }
+    }
+
+    
+    
+
+
 }
